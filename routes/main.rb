173c5d48a0c4b6c2882a20feb223166c5b105de7
Jesse Tomlinson <donjesse@gmail.com>
2014-07-22 22:41:59 -0400

    adjusted routes. added api key in session

--- routes/main.rb
+++ routes/main.rb
@@ -5,10 +5,15 @@ module Applyance
 
         require 'rest_client'
 
-
-        # Home page
+        # Home page (marketing and app)
         app.get '/' do
-          erb :'main/index', :layout => :'layouts/base'
+
+          if session[:api_key] == nil
+            erb :'main/index', :layout => :'layouts/base'
+          else
+            erb :'app/home', :layout => :'layouts/app'
+          end
+
         end
 
         # Spot
@@ -17,18 +22,25 @@ module Applyance
         end
 
         # Spot Application
-        app.get '/spot/:id/apply' do
+        app.get '/apply/:id' do
           erb :'main/spotApp', :layout => :'layouts/base', :locals => {:id => params[:id]}
         end
 
         #
         # Account Stuff
         #
+
+        # Logout
+        app.get '/accounts/logout' do
+          session.clear
+          redirect to ('/')
+        end
+
         # Reviewer Registration
-        app.get '/accounts/register' do
+        app.get '/register' do
           erb :'accounts/register', :layout => :'layouts/base'
         end
-        app.post '/accounts/register' do
+        app.post '/register' do
 
           values = '{
             "account": {
@@ -50,11 +62,10 @@ module Applyance
         end
 
         # Verify Email
-        app.get '/accounts/verify/:id/:verify_digest' do
+        app.get '/accounts/verify' do
 
-          id = params[:id].to_s;
           values = '{
-            "verify_digest": ' + params[:verify_digest] + '
+            "verify_digest": ' + params[:digest] + '
           }'
 
           headers = {
@@ -62,7 +73,7 @@ module Applyance
             :authorization => 'ApplyanceLogin auth=YW55IGNhcm5hbCBwbGVhc3VyZS4='
           }
 
-          RestClient.post 'https://applyance.apiary-mock.com/accounts/' + id + '/verify-email', values, headers
+          RestClient.post 'https://applyance.apiary-mock.com/accounts/verify', values, headers
 
           erb :'accounts/verify', :layout => :'layouts/base', :locals => {:id => params[:id]}
         end
@@ -85,11 +96,10 @@ module Applyance
           RestClient.post('https://applyance.apiary-mock.com/accounts/auth', values, headers){ |response, request, result, &block|
             case response.code
             when 200
-              p "It worked!"
-              response
-              redirect to('/home')
+              api_key = response.headers[:authorization].split('auth=')[1]
+              session[:api_key] = api_key
+              redirect to('/')
             else
-              p "FAIL"
               response.return!(request, result, &block)
             end
           }
@@ -97,10 +107,10 @@ module Applyance
         end
 
         # Reset Password
-        app.get '/accounts/resetpassword' do
+        app.get '/accounts/password/reset' do
           erb :'accounts/resetPassword', :layout => :'layouts/base'
         end
-        app.post '/accounts/resetpassword' do
+        app.post '/accounts/password/reset' do
 
           values = '{
             "email": ' + params[:account][:email].to_s + '
@@ -112,11 +122,9 @@ module Applyance
 
           RestClient.post('https://applyance.apiary-mock.com/accounts/password/reset', values, headers) { |response, request, result, &block|
             case response.code
-            when 200
-              p "It worked!"
+            when 201
               response
             else
-              p "FAIL"
               response.return!(request, result, &block)
             end
           }
@@ -124,34 +132,32 @@ module Applyance
         end
 
         # Set Password
-        app.get '/accounts/setpassword/:id/:token' do
+        app.get '/accounts/password/set' do
           erb :'accounts/setPassword', :layout => :'layouts/base',
             :locals => {:token => params[:token], :id => params[:id]}
         end
-        app.post '/accounts/setpassword' do
+        app.post '/accounts/password/set' do
 
-          id = params[:id].to_s;
-          puts id
           values = '{
             "reset_token": ' + params[:token].to_s + ',
             "new_password": ' + params[:password].to_s + '
           }'
-          puts values
 
           headers = {
             :content_type => 'application/json'
           }
 
-          RestClient.post 'https://applyance.apiary-mock.com/accounts/' + id + '/set-password', values, headers
+          RestClient.post('https://applyance.apiary-mock.com/accounts/passwords/set', values, headers){ |response, request, result, &block|
+            case response.code
+            when 200
+              # api_key = response.headers[:authorization].split('auth=')[1]
+              # session[:api_key] = api_key
+              redirect to('/')
+            else
+              response.return!(request, result, &block)
+            end
+          }
 
-        end
-
-        #
-        # SPA
-        #
-        # Dashboard
-        app.get '/home' do
-          erb :'app/home', :layout => :'layouts/app'
         end
 
       end
