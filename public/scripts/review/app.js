085afae04bc5c2049566e0b2be6d876eb65a0720
Jesse Tomlinson <donjesse@gmail.com>
2014-08-01 23:06:54 -0400

    why isn't this committing with the rest

---
+++ public/scripts/review/app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var angular = require("angular");
+require("angular-route");
+require("moment");
+require("angular-moment");
+
+var ApplyanceApi = require("../services/api");
+
+var Routes = require("./routes");
+
+angular.module('Review', [ApplyanceApi.name, 'ngRoute', 'angularMoment'])
+  .config(['$routeProvider', '$locationProvider', 'me', Routes]);
+
+
+require("./directives");
+require("./filters");
+
+
+require("./services/me");
+require("./services/context");
+require("./services/flash");
+
+require("./controllers/header/header");
+require("./controllers/header/account");
+
+require("./controllers/applications");
+require("./controllers/application");
+require("./controllers/settings");
+require("./controllers/account");
+
+require("./controllers/entities/blueprints");
+require("./controllers/entities/entities");
+require("./controllers/entities/reviewers");
+require("./controllers/entities/settings");
+require("./controllers/entities/labels");
