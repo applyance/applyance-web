f986f9e52bb5d41bde0a1bfd19f942f3df89e9a9
Stephen Watkins <stjowa@gmail.com>
2014-10-14 12:41:02 -0500

    Fixing legacy datums.

--- assets/scripts/review/controllers/application/profile.js
+++ assets/scripts/review/controllers/application/profile.js
@@ -3,5 +3,8 @@
 module.exports = angular.module('Review')
   .controller('ApplicationProfileCtrl', ['$scope', '$filter', '$http',
     function ($scope, $filter, $http) {
-
-    }]);
+      $scope.isLegacyField = function(field) {
+        return moment(field.datum.created_at, 'YYYY-MM-DD HH:mm:ss Z').isBefore('2014-10-13');
+      }
+    }
+  ]);
